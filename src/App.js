6f16256db9936458da5a162766ae5faeaba12075
Reenauud <renaud.raymond43@hotmail.fr>
2023-03-30 18:38:35 +0200

    fix hot reload

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> MAMA SEIGNEUR !!! TOUT RHÉNÉ </p>
+        <p> MAMA SEIGNEUR !!!</p>
       </header>
     </div>
   );
