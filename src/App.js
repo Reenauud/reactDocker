801844a703c3698acf3256b3b89b3231d971079c
Reenauud <renaud.raymond43@hotmail.fr>
2023-03-30 18:42:48 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> MAMA SEIGNEUR !!!</p>
+        <p> test hotreload ok</p>
       </header>
     </div>
   );
